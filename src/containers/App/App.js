843cbc50742d6e563f94574b6e70bef857363d7f
MichelleW31 <mattie.williams31@gmail.com>
2022-12-03 12:50:56 -0700

    update credentials and access token

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,37 +5,21 @@ import Image from "../../components/Image/Image.js";
 import Albums from "../../components/Albums/Albums.js";
 import "./App.scss";
 import axios from "axios";
+import { getApiToken, accessToken } from "../../config.js";
 
 const App = () => {
-  const [access_token, setAccessToken] = useState(null);
   const [artist, setArtist] = useState([]);
   const [step, setStep] = useState(0);
 
   //GETS ACCESS TOKEN FROM CREDENTIALS
   useEffect(() => {
-    let param =
-      "grant_type=client_credentials&client_id=2f473a1d096845c691c2d5217a94a347&client_secret=c524c7aa622e4e65b3cf5f690477bd3a";
-
-    let header = {
-      "Content-Type": "application/x-www-form-urlencoded",
-    };
-
-    axios
-      .post("https://accounts.spotify.com/api/token", param, {
-        headers: header,
-      })
-      .then((response) => {
-        setAccessToken(response.data.access_token);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    getApiToken();
   }, []);
 
   //FINDS ID OF ARTIST SO THAT WE CAN GET TRACKS
   const findID = (artist) => {
     let header = {
-      Authorization: "Bearer " + access_token,
+      Authorization: "Bearer " + accessToken,
     };
 
     axios
@@ -60,7 +44,7 @@ const App = () => {
         return (
           <div className="image-and-albums">
             <Image artist={artist} />
-            <Albums artist={artist} access_token={access_token} />
+            <Albums artist={artist} access_token={accessToken} />
           </div>
         );
       default:
@@ -73,7 +57,7 @@ const App = () => {
   return (
     <div className="App">
       <Header />
-      <Search access_token={access_token} findID={findID} />
+      <Search access_token={accessToken} findID={findID} />
       {artist.length !== 0 ? (
         <h2 className="name">
           {artist.name} - <span className="genre">{artist.genres[0]}</span>
